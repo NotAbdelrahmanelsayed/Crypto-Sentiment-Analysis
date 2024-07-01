4b1026f591dc908e14bb4550400a8042053ac224
NotAbdelrahmanelsayed <bedoelsayed785@gmail.com>
2024-07-01 11:30:24 +0300

    Finishing the scrapper

--- scripts/scrapper.py
+++ scripts/scrapper.py
@@ -1,14 +1,15 @@
-import requests
+import argparse
 import json
 import csv
-from datetime import datetime
 import time
 import os
 
+import requests
+
 
 class Scrapper:
-    secs_in_9_hours = 9 * 60 * 60
-    fields = [
+    SECS_IN_9_HOURS = 9 * 60 * 60
+    FIELDS = [
     'ups', 'retrieved_on', 'selftext', 'media', 'link_flair_text', 'permalink', 'title',
     'secure_media_embed', 'downs', 'thumbnail', 'distinguished', 'domain', 'hide_score',
     'created_utc', 'author_flair_text', 'media_embed', 'subreddit', 'score', 'edited',
@@ -16,26 +17,20 @@ class Scrapper:
     'from', 'author', 'name', 'link_flair_css_class', 'gilded', 'archived', 'id',
     'num_comments', 'author_flair_css_class', 'saved', 'over_18'
 ]
-    
-    def __init__(self, subreddit='Bitcoin', start_epoch=1609459200, today=False, repo=True):
-        self.today = int(time.time()) # Today's unix time
+    START_EPOCH = 1609459200
+    def __init__(self, subreddit, today, path):
+        self.today = int(time.time()) - (24 * 60 * 60) # unix time at yesterday
         self.subreddit = subreddit
-        self.start_epoch = start_epoch if not today else self.today
-        self.repo = repo
+        self.start_epoch = self.START_EPOCH if not today else self.today
+        self.output_file_path = path
 
-    def file_setup(self):
-        if self.repo:
-            self.output_file_path = f"../data/raw/{self.subreddit}_submissions.csv"
-        else:
-            self.output_file_path = f'{self.subreddit}_submissions.csv'
-    
+        os.makedirs(os.path.dirname(self.output_file_path), exist_ok=True)
     def scrape(self):
-        self.file_setup()
         with open(self.output_file_path, "a", encoding='utf-8', newline="") as output_file:
             writer = writer = csv.writer(output_file, escapechar='\\', quoting=csv.QUOTE_ALL)
             file_exist = os.path.isfile(self.output_file_path) and os.path.getsize(self.output_file_path) > 0
             if not file_exist:
-                writer.writerow(self.fields)
+                writer.writerow(self.FIELDS)
             
             current_epoch = self.start_epoch
             file_lines = 0
@@ -44,12 +39,12 @@ class Scrapper:
 
             while current_epoch <= self.today:
                 try:
-                    response = requests.get(f"https://api.pullpush.io/reddit/search/submission/?subreddit={self.subreddit}&after={current_epoch}&before={current_epoch + self.secs_in_9_hours}")
+                    response = requests.get(f"https://api.pullpush.io/reddit/search/submission/?subreddit={self.subreddit}&after={current_epoch}&before={current_epoch + self.SECS_IN_9_HOURS}")
                     if response.status_code == 200:
                         data = response.json()['data']
                         for obj in data:
                             output_obj = []
-                            for field in self.fields:
+                            for field in self.FIELDS:
                                 output_obj.append(obj.get(field, None))
                             writer.writerow(output_obj)
                             file_lines += 1
@@ -64,10 +59,19 @@ class Scrapper:
                 except Exception as e:
                     print(f"Unexpected error for epoch {current_epoch}: {e}")
                 finally:
-                    current_epoch += self.secs_in_9_hours
+                    current_epoch += self.SECS_IN_9_HOURS
 
             print(f"Completed. Total lines: {file_lines}, Bad lines: {bad_lines}")
 
 
-scrapper = Scrapper()
-scrapper.scrape()
+if __name__ == '__main__':
+    # Parse the arguments
+    parser = argparse.ArgumentParser(description='Reddit scrapper')
+    parser.add_argument('--subreddit', default='Bitcoin', help='Specify the subreddit name i.e(Bitcoin..)')
+    parser.add_argument('--today', type=bool, default=False, help='Specify to start scrapping data from yesterday\'s date or from 2021')
+    parser.add_argument('--path', help='path to save the scrapped the data. By default will be saved in data/raw folder')
+    args = parser.parse_args()
+    path = args.path or f"../data/raw/{args.subreddit}_submissions.csv" 
+    
+    scrapper = Scrapper(args.subreddit, args.today, path)
+    scrapper.scrape()
